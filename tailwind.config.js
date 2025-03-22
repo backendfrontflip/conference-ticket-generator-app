ba27a024d39b6ce8d811bd9f3153977053a5bc25
Space Cadet 💻 <154667827+backendfrontflip@users.noreply.github.com>
2025-03-22 21:23:41 +0000

    all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // This makes sure Tailwind scans all React files
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
