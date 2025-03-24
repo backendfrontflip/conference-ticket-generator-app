892b24b3c62bb7fd931866266e4ddbfce6a06e2c
Space Cadet 💻 <154667827+backendfrontflip@users.noreply.github.com>
2025-03-24 08:36:11 +0000

    all files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <img src="/assets/images/logo-full.svg" alt="" className='mx-auto mb-4'/>
+        <img src={`${import.meta.env.BASE_URL}/assets/images/logo-full.svg`} alt="" className='mx-auto mb-4'/>
       <h1 className='font-bold text-2xl mb-10'>Your Journey to Coding <br/> Conf 2025 Starts Here!</h1>
       <p className='mb-10 text-gray-300'>Secure your spot at next year's biggest coding conference.</p>
     </div>
